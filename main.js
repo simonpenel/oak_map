ccff9f65b1afac3094c8e9def3e0db40f2cb25ec
Simon Penel <simon.penel@univ-lyon1.fr>
2024-07-24 11:50:15 +0200

    Les popups marchent

--- main.js
+++ main.js
@@ -1,82 +1,122 @@
-import KML from 'ol/format/KML.js';
-import Map from 'ol/Map.js';
-import StadiaMaps from 'ol/source/StadiaMaps.js';
+import Feature from 'ol/Feature';
 import OSM from 'ol/source/OSM.js';
-import VectorSource from 'ol/source/Vector.js';
-import View from 'ol/View.js';
-import {Heatmap as HeatmapLayer, Tile as TileLayer} from 'ol/layer.js';
+import Point from 'ol/geom/Point';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import {Map, View} from 'ol';
+import {Vector as VectorSource} from 'ol/source';
+import Overlay from 'ol/Overlay.js';
+import {fromLonLat, toLonLat} from 'ol/proj.js';
+import {toStringHDMS} from 'ol/coordinate.js';
 
-const blur = document.getElementById('blur');
-const radius1 = document.getElementById('radius1');
-const radius2 = document.getElementById('radius2');
 
-const vector1 = new HeatmapLayer({
-  source: new VectorSource({
-    url: 'http://localhost:5173/data/2012_Earthquakes_Mag5.kml',
-    format: new KML({
-      extractStyles: false,
-    }),
-  }),
-  blur: parseInt(blur.value, 10),
-  radius1: parseInt(radius1.value, 10),
-  weight: function (feature) {
-    // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
-    // standards-violating <magnitude> tag in each Placemark.  We extract it from
-    // the Placemark's name instead.
-    const name = feature.get('name');
-    const magnitude = parseFloat(name.substr(2));
-    return magnitude - 5;
-  },
+// Definit une source vide
+const source = new VectorSource();
+
+// Definit un client pour la lecture de csv
+const client = new XMLHttpRequest();
+client.open('GET', './data/meteorites.csv');
+client.onload = function () {
+  // Remplit la source
+  const csv = client.responseText;
+  const features = [];
+
+  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
+
+  let curIndex;
+  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
+    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
+    prevIndex = curIndex + 1;
+
+    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
+    if (isNaN(coords[0]) || isNaN(coords[1])) {
+      // guard against bad data
+      continue;
+    }
+
+    features.push(
+      new Feature({
+        name: line[0],
+        mass: parseFloat(line[1]) || 0,
+        year: parseInt(line[2]) || 0,
+        geometry: new Point(coords),
+      })
+    );
+  }
+  source.addFeatures(features);
+};
+client.send();
+
+
+// Definit le layer meteorite
+const meteorites = new VectorLayer({
+  source: source,
 });
 
 
-const vector2 = new HeatmapLayer({
-  source: new VectorSource({
-    url: 'http://localhost:5173/data/2012_Earthquakes_Mag5.kml',
-    format: new KML({
-      extractStyles: false,
-    }),
-  }),
-  blur: parseInt(blur.value, 10),
-  radius2: parseInt(radius2.value, 10),
-  weight: function (feature) {
-    // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
-    // standards-violating <magnitude> tag in each Placemark.  We extract it from
-    // the Placemark's name instead.
-    const name = feature.get('name');
-    const magnitude = parseFloat(name.substr(2));
-    return magnitude - 5;
-  },
-});
-
-const raster = new TileLayer({
-  source: new StadiaMaps({
-    layer: 'stamen_toner',
-  }),
-});
-
-new Map({
-  layers: [raster, vector1,vector2],
+// Definit la carte
+const map = new Map({
   target: 'map',
+  layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+      meteorites,
+  ],
   view: new View({
     center: [0, 0],
     zoom: 2,
   }),
 });
 
-blur.addEventListener('input', function () {
-  vector1.setBlur(parseInt(blur.value, 10));
+
+// Custom
+document
+  .querySelectorAll('.ol-zoom-in, .ol-zoom-out, .ol-rotate-reset')
+  .forEach(function (el) {
+    new bootstrap.Tooltip(el, {
+      container: '#map',
+    });
+  });
+
+
+// Definit position
+const pos = fromLonLat([16.3725, 48.208889]);
+
+// Popup showing the position the user clicked
+const popup = new Overlay({
+  element: document.getElementById('popup'),
+});
+map.addOverlay(popup);
+
+
+
+// Definit element
+const element = document.getElementById('popup');
+
+
+
+map.on('click', function (evt) {
+  const coordinate = evt.coordinate;
+  const hdms = toStringHDMS(toLonLat(coordinate));
+  popup.setPosition(coordinate);
+  const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+      return feature;
+    });
+  console.log(feature.values_);
+  let popover = bootstrap.Popover.getInstance(element);
+  if (popover) {
+    popover.dispose();
+  }
+  popover = new bootstrap.Popover(element, {
+    animation: false,
+    container: element,
+    content: '<p>The location you clicked was:</p><code>' + feature.values_.name + '</code>',
+    html: true,
+    placement: 'top',
+    title: 'Welcome to OpenLayers',
+  });
+  popover.show();
 });
 
-blur.addEventListener('input', function () {
-  vector2.setBlur(parseInt(blur.value, 10));
-});
 
-
-radius1.addEventListener('input', function () {
-  vector1.setRadius(parseInt(radius1.value, 10));
-});
-
-radius2.addEventListener('input', function () {
-  vector2.setRadius(parseInt(radius2.value, 10));
-});
